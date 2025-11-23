d63cf5f1369d7142f4b5f6064485094c122b8a9d
Matias Solis <solisma.dev@gmail.com>
2025-11-23 18:19:14 -0300

    chore(build): auto-update Steam UI example & catalog builds

--- docs/steam_ui/example/flutter_bootstrap.js
+++ docs/steam_ui/example/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1316768927"
+    serviceWorkerVersion: "301913921"
   }
 });
